79f91255041a7fb55ef015c4feb30954e3b3632b
davidcloak <35105111+davidcloak@users.noreply.github.com>
2021-04-18 15:24:21 -0400

    refactor

--- Eport/EportApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EportappConfig(AppConfig):
-    name = 'EportApp'
--- Eport/polls/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def poleMain(request):
-    test= {}
-    test['test'] = "Sea.jpg"
-    return render(request, 'test.html', test)
